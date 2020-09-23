9e07d35742eb4f09e9d6fb18ddbb4b644e012af3
duavudrkd123 <ypgcute@gmail.com>
2020-09-23 13:06:38 +0900

    fsda

--- array2.js
+++ array2.js
@@ -13,3 +13,77 @@ function aryFunc() {
 function callBackFunc(v, i, a) {
     sum += v;
 }
+var num1 = 10;
+var num1 = "hello";
+console.log(num1);
+
+// let num2 = 10; //let은 var보다 문법검사를 열심히 한다.
+let num2 = "hello";
+console.log(num2);
+
+const num3 = 10;
+
+//p1 ,2 ,3 데이터 만들어주고 persons 라는 배열에 값을 넣어주는것
+let p1 = {
+    name: "Hong",
+    age: 20
+}
+let p2 = {
+    name: "lang",
+    age: 25
+}
+let p3 = {
+    name: "choi",
+    age: 30
+}
+let persons = [p1, p2, p3]; //배열을 출력해보는 것
+persons.forEach(function (a, b, c) {
+    // console.log(a, b, c);
+    console.log(a.name + ', ' + a.age);
+});
+
+persons.sort(function (a, b) {
+    if (a.name < b.name) return -1;
+    else return 1;
+});
+
+// numbers = [45,38,66,92,18,100]; //sort하는 방법
+// numbers.sort(function(a, b){
+//     return a-b;
+// });
+// numbers.forEach(function(a){
+// console.log(a)
+// });
+
+persons.forEach(function (a, b, c) {
+    return b.age - a.age;
+});
+persons.forEach((a, b, c) => {
+    console.log('value: ${a.name}, age: ${a.age}, index: ${b.age}');
+});
+numbers = [0, 45, 38, 66, 92, 18, 100];
+numbers.sort(function (a, b, c) {
+ 
+    return a - b;
+});
+console.log(numbers[0]);
+
+let newNum = numbers.map(function (a, b, c) {
+    console.log(`a: ${a}, b: ${b},c: ${c}`);
+    console
+    return a * b; //인덱스랑 곱한거
+});
+console.log("aa" + newNum);
+
+let theNew = newNum.filter(function (a, b, c) {
+    
+    return a > 0;
+});
+console.log(theNew);
+
+let anotherNew = theNew.reduce((a, b, c) => {
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+    
+    return (a > b) ? a : b;
+}, 0);
+console.log(anotherNew);
---
+++ dom.js
@@ -0,0 +1,38 @@
+// dom.js
+
+let days = ['일', '월', '화', '수', '목', '금', '토'];
+// function loadPage() {
+//     let $table = document.createElement("table");
+//     $table.setAttribute('border', '1');
+//     let $tr = document.createElement("tr");
+//     for (let day of days) {
+//         let $th = document.createElement("th"); //element = tag를 말한다
+//         let text = document.createTextNode(day);
+//         $th.appendChild(text);
+//         $tr.appendChild($th);
+//     }
+//     $table.appendChild($tr);
+//     document.getElementById("bdy").appendChild($table);
+// }
+
+
+//forEach로 돌려보기
+function loadPage() {
+    let $table = document.createElement("table");
+    $table.setAttribute('border', '1');
+    let $tr = document.createElement("tr");
+    days.forEach(function(a,b,c){
+        console.log(a, b) {
+            if (b % 7 == 0) {
+                $tr = document.createElement("tr");
+            }
+        }
+
+
+    })
+        $th.appendChild(text);
+        $tr.appendChild($th);
+    }
+    $table.appendChild($tr);
+    document.getElementById("bdy").appendChild($table);
+}
